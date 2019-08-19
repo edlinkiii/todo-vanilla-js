58347198e1def42311eb2fb65886a46b58ad0065
Ed Link <EL099642@codeblue360.com>
2019-08-18 20:19:07 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const container = document.querySelector('#container');
+
+container.innerHTML = `<h1>To Do -- Vanilla JS</h1>`;
+
+// setup node.js server
+// connect to todo api (GET)
+// format/display todo items
+// add `check` functionality (PUT)
+// add `delete` functionality (DELETE)
+// create `add` form
+// handle `add` form input (POST)
+
+
+
+/******* NEXT
+ * Vanilla Web Components
+ * React
+ * React/Redux
+ * Angular
+ * Backbone
+ * TypeScript 
+*/
